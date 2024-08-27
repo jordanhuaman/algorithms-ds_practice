451326dab3d3863509a1d82866617267bc51892a
jordan <jordanelcracken@gmail.com>
2024-08-27 12:18:26 -0500

    feat: add chap_3

---
+++ books-practice/Data Structures and  Algorithms in Java/src/chap_3/arrays/Scoreboard.java
@@ -0,0 +1,36 @@
+package chap_3.arrays;
+
+public class Scoreboard {
+  private int numEntries = 0;
+  private GameEntry[] board;
+
+  public Scoreboard(int capacity) {
+    board = new GameEntry[capacity];
+  }
+
+  public void printPlayers() {
+    for (GameEntry gameEntry : board) {
+      System.out.println(gameEntry.getName() + "score: " + gameEntry.getScore());
+    }
+  }
+
+  public void getLenght() {
+    System.out.println(board.length);
+  }
+
+  public void addEntry(GameEntry e) {
+    int newScore = e.getScore();
+    if (numEntries < board.length || newScore > board[numEntries - 1].getScore()) {
+      if (numEntries < board.length) {
+        numEntries++;
+      }
+      int j = numEntries - 1;
+
+      while (j > 0 && board[j - 1].getScore() < newScore) {
+        board[j] = board[j - 1];
+        j--;
+      }
+      board[j] = e;
+    }
+  }
+}
