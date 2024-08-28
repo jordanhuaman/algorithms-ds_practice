751d0d4b48f81dcc738b721a84969c77f2562ed3
jordan <jordanelcracken@gmail.com>
2024-08-28 15:04:48 -0500

    feat: add delete method

--- books-practice/Data Structures and  Algorithms in Java/src/chap_3/arrays/Scoreboard.java
+++ books-practice/Data Structures and  Algorithms in Java/src/chap_3/arrays/Scoreboard.java
@@ -25,12 +25,27 @@ public class Scoreboard {
         numEntries++;
       }
       int j = numEntries - 1;
-
       while (j > 0 && board[j - 1].getScore() < newScore) {
+        System.out.println("Aqui");
         board[j] = board[j - 1];
         j--;
       }
       board[j] = e;
     }
+  }
+
+  public GameEntry remove(int i) throws IndexOutOfBoundsException {
+    if (i < 0 || i > numEntries) {
+      throw new IndexOutOfBoundsException("Index invalid: " + i);
+    }
+    GameEntry temp = board[i];
+    for (int j = i; j < numEntries - 1; j++) {
+      // TODO Reasignando todo a partir del incide ingresado
+      board[j] = board[j + 1];
+    }
+    // TODO por consecuencia, si eliminamos uno el final sera nullo
+    board[numEntries - 1] = null;
+    numEntries--;
+    return temp;
   }
 }
