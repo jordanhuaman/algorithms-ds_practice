226a3f883153a5631be63610e7d530ebb1710ff7
jordan <jordanelcracken@gmail.com>
2024-09-12 08:54:30 -0500

    update: message to guide getClass()

--- books-practice/Data Structures and  Algorithms in Java/src/chap_3/testArrays/Test.java
+++ books-practice/Data Structures and  Algorithms in Java/src/chap_3/testArrays/Test.java
@@ -7,10 +7,15 @@ public class Test<T> {
     return array1.equals(array2);
   }
 
-  public boolean DeepEquslMethod(T array1, T array2) {
+  public boolean DeepEquslMethod(T array1, T array2, Object o) {
     int[][] test = { { 1, 2 }, { 3, 4 } };
     if (array1.getClass() == test.getClass()) {
-      System.out.println("trueeeeeeeeeeeeeeeeeeeeeee");
+
+      // ? getClass return -> class chap_3.testArrays.Test
+      // ? Thus o.getClass() sould return -> class chap_3.testArrays.Test
+      // TODO so, o must be a Test object
+      System.out.println(getClass());
+      System.out.println(o.getClass());
       return Arrays.deepEquals((Object[]) array1, (Object[]) array2);
     }
     return false;
@@ -22,6 +27,6 @@ public class Test<T> {
     int[][] array2 = { { 1, 2 }, { 3, 4 } };
 
     System.out.println(result.EqualsMethod(array1, array2));
-    System.out.println(result.DeepEquslMethod(array1, array2));
+    System.out.println(result.DeepEquslMethod(array1, array2, result));
   }
 }
