df85518b1b4ffb978abd668f273644db77416d66
jordanhuaman <jordanelcracken@gmail.com>
2026-04-03 10:01:46 -0500

    feat: chap-5 add binary search implementations in Java, Python, and TypeScript

---
+++ books-practice/Data Structures and  Algorithms in Java/src/chap_5/binary_search/BSearch.java
@@ -0,0 +1,26 @@
+package chap_5.binary_search;
+
+public class BSearch {
+  public static void main(String[] args) {
+    int[] array = { 10, 11, 20, 22, 30, 50 };
+    int result = run(array, 100, 0, array.length - 1);
+    System.out.println(result);
+  }
+
+  public static int run(int[] arr, int n, int low, int high) {
+    if (low > high) {
+      return -1;
+    }
+    int middle = (low + high) / 2;
+
+    if (arr[middle] == n) {
+      return middle;
+    }
+
+    if (arr[middle] > n) {
+      return run(arr, n, low, middle - 1);
+    } else {
+      return run(arr, n, middle + 1, high);
+    }
+  }
+}
---
+++ books-practice/Data Structures and  Algorithms in Java/src/chap_5/binary_search/BSearch.ts
@@ -0,0 +1,24 @@
+const search = (arr: number[], target: number, low: number, higher: number): number => {
+
+  if (low > higher) {
+    return -1
+  }
+
+  var middle: number = Math.round((low + higher)/2);
+    console.log(middle)
+
+  if (arr[middle] == target) {
+    return middle;
+  }
+
+  if (arr[middle] > target) {
+    console.log(arr[middle])
+    return search(arr, target, low, middle-1);
+  }
+  else {
+    return search(arr, target, middle+1, higher);
+  }
+}
+
+const result = search([1,10,20,21,30], 1, 0, 4);
+console.log(result);
